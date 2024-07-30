71fef3ca34e340c2c36748a3a589277c4565b5e5
Christopher Hahn <chahn970@gmail.com>
2024-07-30 16:58:58 -0600

    write computer choice function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+function getComputerChoice(){
+    const options = ['rock', 'paper', 'scissors'];
+    let weaponSelector = Math.ceil(Math.random() * 3 - 1)
+    return options[weaponSelector];
+}
