b0c68b3cf6b149fe6422f5291976a64de21ac4c2
Christopher Hahn <chahn970@gmail.com>
2024-07-30 17:47:07 -0600

    write human choice function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+const buttons = document.querySelectorAll('button');
+let humanScore = 0; 
+let computerScore = 0;
+
 function getComputerChoice(){
     const options = ['rock', 'paper', 'scissors'];
     let weaponSelector = Math.ceil(Math.random() * 3 - 1)
     return options[weaponSelector];
 }
+
+ function getHumanChoice(){
+   let humanSelection = this.dataset.item;
+   return humanSelection;
+    
+ }
+
+
+
+
+buttons.forEach(button => button.addEventListener('click', getHumanChoice));
